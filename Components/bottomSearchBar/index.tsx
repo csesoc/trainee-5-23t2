d7552b332b3a29a84826b5a29cc5bfc8c1c61c39
matthew-lim-matthew-lim <matthewlimcenterdiv@gmail.com>
2023-09-05 13:15:13 +1000

    added components

---
+++ Components/bottomSearchBar/index.tsx
@@ -0,0 +1,15 @@
+import './index.scss';
+import React, { useEffect, useState } from 'react';
+
+const BottomSearchBar = () => {
+    return (
+        <div className="flex flex-col">
+            <div className="flex"> hi </div>
+            <div className="flex"> hi2 </div>
+            
+            HI!
+        </div>
+    );
+};
+
+export default BottomSearchBar;
