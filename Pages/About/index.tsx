03252f89a366aff715d7af2c965204c6a40d9d94
matthew-lim-matthew-lim <matthewlimcenterdiv@gmail.com>
2023-09-05 13:14:51 +1000

    added pages

---
+++ Pages/About/index.tsx
@@ -0,0 +1,18 @@
+import './index.scss';
+import React from 'react';
+
+/* const aboutPage = () => {
+    return (
+        <div>
+            HI!
+        </div>
+    );
+}; */
+
+export default function About() {
+    return (
+        <div>
+            ABOUTPAGE!
+        </div>
+    );
+}
---
+++ Pages/Home/index.tsx
@@ -0,0 +1,20 @@
+import './index.scss';
+import React from 'react';
+import BottomSearchBar from '../../Components/BottomSearchBar';
+
+/* const homePage = () => {
+    return (
+        <div>
+            HI!
+        </div>
+    );
+}; */
+
+export default function Home() {
+    return (
+        <div>
+            HOMEPAGE!
+            <BottomSearchBar></BottomSearchBar>
+        </div>
+    );
+};
