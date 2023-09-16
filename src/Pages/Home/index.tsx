84b52610601368506a3749b557ecfb4dd26441e6
Matthew Lim <matthewlimcenterdiv@gmail.com>
2023-09-16 22:28:38 +1000

    allow users to navigate between pages

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import BottomSearchBar from '../../Components/bottomSearchBar';
 import MapView from '../../Components/MapView';
 import { API_KEY } from "../../config";
+import { Link } from 'react-router-dom';
 
 
 /* const homePage = () => {
@@ -16,6 +17,15 @@ import { API_KEY } from "../../config";
 export default function Home() {
     return (
         <div className='map-container'>
+            <Link to="/login">
+                <button>Login</button>
+            </Link>
+            <Link to="/signup">
+                <button>Sign Up</button>
+            </Link>
+            <Link to="/about">
+                <button>About</button>
+            </Link>
             <MapView/>
         </div>
     );
