b33e106d17ae110d9bebb58037c7271f55fd3662
Matthew Lim <matthewlimcenterdiv@gmail.com>
2023-09-16 22:44:31 +1000

    buttons on div in front

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -16,17 +16,21 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div className='map-container'>
-            <Link to="/login">
-                <button>Login</button>
-            </Link>
-            <Link to="/signup">
-                <button>Sign Up</button>
-            </Link>
-            <Link to="/about">
-                <button>About</button>
-            </Link>
-            <MapView/>
-        </div>
+        <>
+            <div className='button-container m-2' style={{ zIndex: 1, position: 'fixed'}}>
+                <Link to="/login">
+                    <button>Login</button>
+                </Link>
+                <Link to="/signup">
+                    <button>Sign Up</button>
+                </Link>
+                <Link to="/about">
+                    <button>About</button>
+                </Link>
+            </div>
+            <div className='map-container' style={{ zIndex: -1 }}>
+                <MapView/>
+            </div>
+        </>
     );
 }
