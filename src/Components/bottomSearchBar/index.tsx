2c2909a9fe91881a5098a6cd6c9d756e0ceff9eb
Matthew Lim <matthewlimcenterdiv@gmail.com>
2023-09-17 01:00:29 +1000

    fixed the searchbar, also made it move up when text is input to reveal the other menu

--- src/Components/bottomSearchBar/index.tsx
+++ src/Components/bottomSearchBar/index.tsx
@@ -20,26 +20,37 @@ const BottomSearchBar = () => {
   
     return (
       <div className={`fixed bottom-0 left-0 right-0 bg-gray-800 p-4 flex items-center justify-between bg-gray-400 transition-all ${isMenuOpen ? 'translate-y-0' : 'translate-y-full'}`}>
-        <div className="max-w-screen-lg mx-auto">
-        <div className="flex items-center justify-between">
+        <div className="fixed bottom-0 left-0 right-0 bg-gray-800">
+          <div className="max-w-screen-lg mx-auto m-4">
+          <div className="flex items-center justify-between">
 
-            <div className="flex w-full">
-            <input
-                type="text"
-                className="w-full bg-gray-900 text-white rounded-md py-2 px-3 outline-none mr-2"
-                placeholder="Search on campus..."
-                value={searchInput}
-                onChange={handleInputChange}
-            />
-            <button
-                className="bg-blue-500 hover:bg-blue-600 text-white rounded-md py-2 px-4 mx-1"
-                onClick={handleSearchSubmit}
-            >
-                Search
-            </button>
-            </div>
+              <div className="flex w-full">
+              <input
+                  type="text"
+                  className="w-full bg-gray-900 text-white rounded-md py-2 px-3 outline-none mr-2"
+                  placeholder="Search on campus..."
+                  value={searchInput}
+                  onChange={handleInputChange}
+              />
+              <button
+                  className="bg-blue-500 hover:bg-blue-600 text-white rounded-md py-2 px-4 mx-1"
+                  onClick={handleSearchSubmit}
+              >
+                  Search
+              </button>
+              </div>
 
-            </div>
+              </div>
+
+          </div>
+            
+          <div
+            className={`bg-gray-800 p-4 flex items-center justify-between bg-gray-400 h-100 ${
+              isMenuOpen ? 'block' : 'hidden' // Use block to show, hidden to hide
+            }`}
+          >
+            <div className='text-white'>hmu</div>
+          </div>
 
         </div>
       </div>
