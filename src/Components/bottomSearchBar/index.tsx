52f273e08e23fe62a67da65a54e6907f235d3285
Matthew Lim <matthewlimcenterdiv@gmail.com>
2023-09-17 00:42:52 +1000

    fixed the searchbar, also made it move up when text is input to reveal the other menu

--- src/Components/bottomSearchBar/index.tsx
+++ src/Components/bottomSearchBar/index.tsx
@@ -4,10 +4,12 @@ import React, { useEffect, useState } from 'react';
 const BottomSearchBar = () => {
     // Add state to manage search input
     const [searchInput, setSearchInput] = useState('');
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
   
     // Function to handle search input changes
     const handleInputChange = (e) => {
       setSearchInput(e.target.value);
+      setIsMenuOpen(!!e.target.value);
     };
   
     // Function to handle search submission
@@ -17,18 +19,17 @@ const BottomSearchBar = () => {
     };
   
     return (
-      <div className="fixed bottom-0 left-0 right-0 bg-gray-800 p-4">
+      <div className={`fixed bottom-0 left-0 right-0 bg-gray-800 p-4 flex items-center justify-between bg-gray-400 transition-all ${isMenuOpen ? 'translate-y-0' : 'translate-y-full'}`}>
         <div className="max-w-screen-lg mx-auto">
-        <div className="flex items-center justify-between bg-gray-400">
+        <div className="flex items-center justify-between">
 
-            <div className="flex md:w-3/4">
+            <div className="flex w-full">
             <input
                 type="text"
-                className="w-full bg-gray-900 text-white rounded-md py-2 px-3 outline-none"
+                className="w-full bg-gray-900 text-white rounded-md py-2 px-3 outline-none mr-2"
                 placeholder="Search on campus..."
                 value={searchInput}
                 onChange={handleInputChange}
-                
             />
             <button
                 className="bg-blue-500 hover:bg-blue-600 text-white rounded-md py-2 px-4 mx-1"
