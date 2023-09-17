b57504d392d80ff130880df88f0ede7b5c77ea20
Matthew Lim <matthewlimcenterdiv@gmail.com>
2023-09-17 12:32:14 +1000

    added favourites scrollbox

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -31,37 +31,75 @@ export default function Home() {
                     <button>Settings</button>
                 </Link>
             </div>
+
             <div className='sidebar-container w-1/5' style={{zIndex: 1, position: 'fixed', top:0, right:0}}>
-                <div className='sidebar flex flex-col h-60 bg-white rounded-md m-2 overflow-y-scroll'>
+                <div className='sidebar flex flex-col h-60 bg-white rounded-md m-2'>
                     <div className='m-2 font-bold'>
                         Recents
                     </div>
-                    <div className='m-2'>
-                        UNSW Kensington
-                    </div>
-                    <div className='m-2'>
-                        Sydney Opera House
-                    </div>
-                    <div className='m-2'>
-                        Hmu
-                    </div>
-                    <div className='m-2'>
-                        Place 
-                    </div>
-                    <div className='m-2'>
-                        Place
-                    </div>
-                    <div className='m-2'>
-                        Place
-                    </div>
-                    <div className='m-2'>
-                        Place
-                    </div>
-                    <div className='m-2'>
-                        Place
+                    <div className = 'overflow-y-scroll'>
+                        <div className='m-2'>
+                            UNSW Kensington
+                        </div>
+                        <div className='m-2'>
+                            Sydney Opera House
+                        </div>
+                        <div className='m-2'>
+                            Hmu
+                        </div>
+                        <div className='m-2'>
+                            Place 
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <div className='sidebar-container w-1/5' style={{zIndex: 1, position: 'fixed', top:255, right:0}}>
+                <div className='sidebar flex flex-col h-60 bg-white rounded-md m-2'>
+                    <div className='m-2 font-bold'>
+                        Favourites
+                    </div>
+                    <div className = 'overflow-y-scroll'>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place 
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                        <div className='m-2'>
+                            Place
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className='map-container' style={{ zIndex: 0, position: 'fixed', top: 0, left: 0}}>
                 <MapView/>
             </div>
